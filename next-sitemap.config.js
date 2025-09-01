eda261fbdc00f20c8d8eae53dad34f27e851f7b6
Rakibul Alam <rakibulalam59900@gmail.com>
2025-09-01 22:31:12 +0600

    Edit site map

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,16 +1,17 @@
 // next-sitemap.config.js
 module.exports = {
-    siteUrl: 'https://basirannesahighschool.edu.bd',
-    generateRobotsTxt: true,
-    sitemapSize: 5000,
-    changefreq: 'weekly',
-    priority: 0.7,
-    // manually add your App Router URLs
-    additionalPaths: async (config) => [
-      { loc: '/' },
-      { loc: '/about' },
-      { loc: '/contact' },
-      { loc: '/admin/auth/student' },
-    ],
-  };
-  
+  siteUrl: 'https://basirannesahighschool.edu.bd',
+  generateRobotsTxt: true,
+  sitemapSize: 5000,
+  changefreq: 'weekly',
+  priority: 0.7,
+  // manually add your App Router URLs
+  additionalPaths: async (config) => [
+    { loc: '/' },
+    { loc: '/about' },
+    { loc: '/achievements' },
+    { loc: '/teacher' },
+    { loc: '/student' },
+    { loc: '/admin/login' },
+  ],
+};
